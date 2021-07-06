c28af604029890a5fd4a61e1d4386c6b13a4c5f2
Jesen Meznarich <jesenmeznarich@mail.weber.edu>
2021-07-06 13:44:00 -0400

    Added BeautifulSoup and Scraping

--- WebScraper.py
+++ WebScraper.py
@@ -1,3 +1,30 @@
+#!/usr/bin/python3
+
+# Check to see if a bird is on sale at Murray McMurray Hatchery
+# and alert the user when it is
+
+
+# Imports
 import requests
+from bs4 import BeautifulSoup
+
+#make a request from the website
 page = requests.get("https://www.mcmurrayhatchery.com/juvenile_mandarin_ducks.html")
-print(page)
+
+# create an instance of the beautifulSoup
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# find the sold out class
+soldOut = []
+soldOut = soup.find_all('span', class_='prod_is_bo_soldout')
+
+print(soldOut)
+
+# not sold out
+if not soldOut:
+    print("item in stock")
+# sold out
+else:
+    print("sold out")
+
+#print(soup.prettify())
