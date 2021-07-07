39b1bd1423987d5d62137723c5c7e0cbbb2409c5
Jesen Meznarich <jesenmeznarich@mail.weber.edu>
2021-07-06 22:20:17 -0400

    Added a notify function to notify the user

--- WebScraper.py
+++ WebScraper.py
@@ -7,6 +7,28 @@
 # Imports
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+
+
+# Function that you can notify however you want
+# Currently it lets you sent a simple email throught a gmail account
+# To use on sender email turn on "Less secure app access"
+def notify():
+    sender = "from@from.com"
+    password = "password"
+    receiver = "t0@to.com"
+    message = "enter message"
+
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+
+    server.login(sender, password)
+
+    server.sendmail(sender, receiver, message)
+    server.quit()
+
+
+
 
 #make a request from the website
 page = requests.get("https://www.mcmurrayhatchery.com/juvenile_mandarin_ducks.html")
@@ -18,13 +40,9 @@ soup = BeautifulSoup(page.content, 'html.parser')
 soldOut = []
 soldOut = soup.find_all('span', class_='prod_is_bo_soldout')
 
-print(soldOut)
-
 # not sold out
 if not soldOut:
     print("item in stock")
-# sold out
+    notify()
 else:
     print("sold out")
-
-#print(soup.prettify())
