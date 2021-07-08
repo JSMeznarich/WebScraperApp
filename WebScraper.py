b22631d6e12024d8e83bec210839c505528f6f46
Jesen Meznarich <jesenmeznarich@mail.weber.edu>
2021-07-07 23:26:45 -0400

    Created checkStock function and implemented a schedule

--- WebScraper.py
+++ WebScraper.py
@@ -8,6 +8,10 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import time
+
+# To install use "$ pip install scheduel"
+import schedule
 
 
 # Function that you can notify however you want
@@ -28,21 +32,37 @@ def notify():
     server.quit()
 
 
+# Function that will get a web page and check the stock
+def checkStock():
+    # make a request from the website
+    page = requests.get("https://www.mcmurrayhatchery.com/juvenile_mandarin_ducks.html")
+
+    # create an instance of the beautifulSoup
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # find the sold out class
+    soldOut = []
+    soldOut = soup.find_all('span', class_='prod_is_bo_soldout')
+
+    # not sold out
+    if not soldOut:
+        print("item in stock")
+        notify()
+    else:
+        print("sold out")
+
+# Schedule to run at a certain time
+schedule.every().day.at("08:00").do(checkStock)
+schedule.every().day.at("12:00").do(checkStock)
+schedule.every().day.at("16:00").do(checkStock)
+schedule.every().day.at("20:00").do(checkStock)
+
+# Loop so that the scheduling task
+# keeps on running all time
+while True:
+
+    # Checks every hour to see if a scheduled task is pending
+    schedule.run_pending()
+    time.sleep(3600)
 
 
-#make a request from the website
-page = requests.get("https://www.mcmurrayhatchery.com/juvenile_mandarin_ducks.html")
-
-# create an instance of the beautifulSoup
-soup = BeautifulSoup(page.content, 'html.parser')
-
-# find the sold out class
-soldOut = []
-soldOut = soup.find_all('span', class_='prod_is_bo_soldout')
-
-# not sold out
-if not soldOut:
-    print("item in stock")
-    notify()
-else:
-    print("sold out")
